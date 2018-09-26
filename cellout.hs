4c4d55fcecd3cfcfd21f0134878b5e3a80be03f4
Paul Ivanov <pivanov5@bloomberg.net>
2018-09-25 20:51:23 -0700

    whoa, turns out i can actually do this

--- cellout.hs
+++ cellout.hs
@@ -1,19 +1,40 @@
 
 import Text.ParserCombinators.ReadP
 import Data.List
+import Data.Set
 
 data Notebook = 
     Notebook 
     { filename :: String
     , cells :: [ Cell ]
+    , nbmetadata :: Set String
     } deriving Show
 
-data Cell = MarkdownCell | CodeCell | RawCell
+data CommonCellContent = 
+    CommonCellContent 
+    { source :: [String]
+    , metadata :: Set String
+    } deriving Show
+
+data Cell 
+    = MarkdownCell CommonCellContent 
+    | CodeCell  CommonCellContent
+    -- | RawCell CellContent
     deriving Show
 
+-- let's think about this a bit, I'll be able to case-switch on cell type if I
+-- go with the above, but is that something I will want to do? I guess it makes
+-- the rest of the validation more explicit
+--
 
 testNb :: Notebook
-testNb = Notebook "hallo.ipynb" [MarkdownCell, CodeCell, CodeCell]
+testNb = Notebook "hallo.ipynb" 
+    [ MarkdownCell $ CommonCellContent ["yo"] empty
+    , CodeCell $ CommonCellContent ["print ('hello')"] empty
+    , CodeCell $ CommonCellContent ["print ('goodbye')"] empty
+    ]
+    empty
+
 main :: IO ()
 main = do
     putStr (show testNb)
