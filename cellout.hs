aaaa273b72a9faa8616d7619db970bd2e6313c56
Paul Ivanov <pivanov5@bloomberg.net>
2018-09-25 20:27:53 -0700

    got notebook structure

--- cellout.hs
+++ cellout.hs
@@ -1,6 +1,19 @@
 
 import Text.ParserCombinators.ReadP
+import Data.List
 
 data Notebook = 
-    Notebook { filename :: String
-        } deriving Show
+    Notebook 
+    { filename :: String
+    , cells :: [ Cell ]
+    } deriving Show
+
+data Cell = MarkdownCell | CodeCell | RawCell
+    deriving Show
+
+
+testNb :: Notebook
+testNb = Notebook "hallo.ipynb" [MarkdownCell, CodeCell, CodeCell]
+main :: IO ()
+main = do
+    putStr (show testNb)
