535bcd4a66f3ea60d75c1d5f0bb854fbc8f7f900
Paul Ivanov <pivanov5@bloomberg.net>
2018-11-08 20:40:25 -0800

    yes! building onward and awkward!

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
+import System.Environment (getArgs)
+
 import Lib
 import Cellout
 
 main :: IO ()
-main = someFunc
+main =  do
+    args <- getArgs
+    case args of
+        [input] -> stripOutputIO input "no_output.ipynb"
+        [input, output] -> stripOutputIO input output
+        _ -> putStrLn "please specify at least the input filename"
