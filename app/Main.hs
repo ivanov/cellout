952852dfb2e5c1fe8a5cbc0e01fa0a0111231f7b
Paul Ivanov <pivanov5@bloomberg.net>
2019-04-29 17:45:48 -0700

    version string

--- app/Main.hs
+++ app/Main.hs
@@ -77,7 +77,7 @@ main = do
                 (fullDesc <> progDesc "Convert to notebook formats" <>
                  header "cellout - buy into rich notebook conversion")
         versionOption :: Parser (a -> a)
-        versionOption = infoOption "0.1.1" (long "version" <> help "show Version")
+        versionOption = infoOption "0.1.1-redis" (long "version" <> help "show Version")
         programOptions :: Parser Opts
         programOptions =
             Options
